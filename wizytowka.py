59aa7025b878c42e3f9eed18c3f4333439cf7495
Przemysław Czajka <zamaskowany@o2.pl>
2022-10-23 11:41:12 +0200

    faker implement

--- wizytowka.py
+++ wizytowka.py
@@ -1,5 +1,13 @@
 #Wizytówka
-import faker
+from faker import Faker
+def create_contacts(type, amount):
+    fake = Faker()
+    if type == BaseContact:
+        for i in range(amount):
+            return(f"Osoba:",fake.name(),"Email:",fake.email(),"Telefon:",fake.phone_number())
+    elif type == BusinessContact:
+        for i in range(amount):
+            return(f"Osoba:",fake.name(),"Email: ",fake.email(),"Telefon służbowy:",fake.phone_number(),"Firma:",fake.company(),"Stanowisko:",fake.job(),)
 class Card:
     def __init__(self, name, surname, company, position, email):
         self.name = name
@@ -21,16 +29,23 @@ class BaseContact(Card):
         return f'Wybieram numer: {self.phone} i dzwonię do: {self.name} {self.surname}'
     def label_length(self):
         return len({self.name} + {self.surname})
-    
+class BusinessContact(Card):
+    def __init__(self, phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.phone = phone
+    def contact(self):
+        return f'Wybieram numer: {self.phone} i dzwonię do: {self.name} {self.surname}'
+    def label_length(self):
+        return len({self.name} + {self.surname})
 card1 = Card(name="Przemek", surname="Czajka", company="Reganta", position="Printer",  email="przem@google.com")
-card2 = Card(name="Ela", surname="Czajka", company="Nordea", position="Process oficer", phone="333345672", email="ela@google.com")
+#card2 = Card(name="Ela", surname="Czajka", company="Nordea", position="Process oficer", phone="333345672", email="ela@google.com")
 card3 = Card(name="Basia", surname="Czajka", company="Szkoła", position="Uczeń", email="basia@google.com")
 card4 = Card(name="Wojtek", surname="Czajka", company="Szkoła", position="Uczeń", email="wojo@google.com")
-card5 = Card(name="Tomek", surname="Kowalski", company="Maszyny", position="Lider", phone="333905771",email="tom@google.com")
-card6 = Card(name="Remek", surname="Iksiński", company="Mosty", position="Operator", phone="333665339",email="rem@google.com")
-card7 = Card(name="Lidka", surname="Wolska", company="Stars", position="Junior", phone="333549066",email="lid@google.com")
-card8 = Card(name="Wiola", surname="Czabańska", company="Ruby", position="Developer", phone="333773009",email="wiola@google.com")
-card9 = Card(name="Zbyszek", surname="Nowak", position="Senior", email="zbych@google.com")
+#card5 = Card(name="Tomek", surname="Kowalski", company="Maszyny", position="Lider", phone="333905771",email="tom@google.com")
+#card6 = Card(name="Remek", surname="Iksiński", company="Mosty", position="Operator", phone="333665339",email="rem@google.com")
+#card7 = Card(name="Lidka", surname="Wolska", company="Stars", position="Junior", phone="333549066",email="lid@google.com")
+#card8 = Card(name="Wiola", surname="Czabańska", company="Ruby", position="Developer", phone="333773009",email="wiola@google.com")
+#card9 = Card(name="Zbyszek", surname="Nowak", position="Senior", email="zbych@google.com")
 #cards = [card1, card2, card3, card4, card5, card6, card7, card8]
 #by_name = sorted(cards, key=lambda card: card.name)
 #by_company = sorted(cards, key=lambda card: card.company)
@@ -40,3 +55,4 @@ card9 = Card(name="Zbyszek", surname="Nowak", position="Senior", email="zbych@go
 #print(by_name)
 #print(by_company)
 #print(BaseContact)
+create_contacts(BusinessContact, 1)
