2d4d4eb354f763d5b3ca2ced10a0d093c82fc389
Przemysław Czajka <zamaskowany@o2.pl>
2022-10-23 14:06:27 +0200

    styling

--- wizytowka.py
+++ wizytowka.py
@@ -4,10 +4,11 @@ def create_contacts(type, amount):
     fake = Faker()
     if type == BaseContact:
         for i in range(amount):
-            return(f"Osoba:",fake.name(),"Email:",fake.email(),"Telefon:",fake.phone_number())
+            print(f"Osoba:",fake.name(),"Email:",fake.email(),"Telefon:",fake.phone_number())
     elif type == BusinessContact:
         for i in range(amount):
-            return(f"Osoba:",fake.name(),"Email: ",fake.email(),"Telefon służbowy:",fake.phone_number(),"Firma:",fake.company(),"Stanowisko:",fake.job(),)
+            print(f"Osoba:",fake.name(),"Email: ",fake.email(),"Telefon służbowy:",fake.phone_number(),"Firma:",fake.company(),"Stanowisko:",fake.job(),)
+    
 class Card:
     def __init__(self, name, surname, company, position, email):
         self.name = name
@@ -22,16 +23,16 @@ class Card:
     def contact(self):
         return f'Kontaktuję się z: {self.name} {self.surname} {self.company}'
 class BaseContact(Card):
-    def __init__(self, name, surname, phone, email):
-        super().__init__(name, surname, email)
+    def __init__(self, phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.phone = phone
     def contact(self):
         return f'Wybieram numer: {self.phone} i dzwonię do: {self.name} {self.surname}'
     def label_length(self):
         return len(self.name) + len(self.surname)
 class BusinessContact(Card):
-    def __init__(self, name, surname, company, position, phone, email, corp_phone,*args, **kwargs):
-        super().__init__(name, surname, company, position, email)
+    def __init__(self,phone, corp_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.phone = phone
         self.corp_phone = corp_phone
     def contact(self):
@@ -40,21 +41,19 @@ class BusinessContact(Card):
         return len(self.name) + len(self.surname)
 card1 = BusinessContact(name="Przemek", surname="Czajka", company="Reganta", position="Printer", phone="333905771", email="przem@google.com", corp_phone="555311221")
 card2 = BusinessContact(name="Ela", surname="Czajka", company="Nordea", position="Process oficer", phone="333345672", email="ela@google.com", corp_phone="555311221")
-#card3 = BaseContact(name="Basia", surname="Czajka", phone="333905771", email="basia@google.com")
-#card4 = BaseContact(name="Wojtek", surname="Czajka", phone="333905771", email="wojo@google.com",)
+card3 = BaseContact(name="Basia", surname="Czajka", company="Szkoła", position="Uczeń", phone="333905771", email="basia@google.com")
+card4 = BaseContact(name="Wojtek", surname="Czajka", company="Szkoła", position="Uczeń", phone="333905771", email="wojo@google.com")
 card5 = BusinessContact(name="Tomek", surname="Kowalski", company="Maszyny", position="Lider", phone="333905771",email="tom@google.com", corp_phone="555311221")
 card6 = BusinessContact(name="Remek", surname="Iksiński", company="Mosty", position="Operator", phone="333665339",email="rem@google.com", corp_phone="555311221")
 card7 = BusinessContact(name="Lidka", surname="Wolska", company="Stars", position="Junior", phone="333549066",email="lid@google.com", corp_phone="555311221")
 card8 = BusinessContact(name="Wiola", surname="Czabańska", company="Ruby", position="Developer", phone="333773009",email="wiola@google.com", corp_phone="555311221")
-#card9 = BaseContact(name="Zbyszek", surname="Nowak", phone="333905771", email="zbych@google.com")
-#cards = [card1, card2, card3, card4, card5, card6, card7, card8]
-#by_name = sorted(cards, key=lambda card: card.name)
-#by_company = sorted(cards, key=lambda card: card.company)
+card9 = BaseContact(name="Zbyszek", surname="Nowak", company="Home", position="Senior", phone="333665339", email="zbych@google.com")
 
-print(card1.label_length())
-#print(by_age)
-#print(by_name)
-#print(by_company)
-#print(BaseContact)
+#print(card1.contact())
+#print(card1.label_length())
+#print(card3.contact())
+#print(card3.label_length())
+#create_contacts(BaseContact, 5)
+#create_contacts(BusinessContact, 5)
 
-#create_contacts(BusinessContact, 1)
+
